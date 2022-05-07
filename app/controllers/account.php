a77e94fe73193bc7778ada2afef8c6680f506896
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-07 20:56:12 +0530

    Added required controller classes

---
+++ app/controllers/account.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . "/../utils.php";
+require_once __DIR__ . "/../models/UserManager.php";
+
+class account extends Controller
+{
+    
+}
---
+++ app/controllers/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . "/../utils.php";
+require_once __DIR__ . "/../models/UserManager.php";
+
+class admin extends Controller
+{
+
+}
