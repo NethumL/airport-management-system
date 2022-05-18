e088c0afbd81dafacace99afcc087e7dfc8f045e
Laksika Tharmalingam <laksi1999@gmail.com>
2022-05-18 10:12:16 +0530

    Navbar edited

--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?php echo htmlspecialchars(BASE_URL . 'home/index') ?>">
-            Airport Management System
+        <a class="navbar-brand rounded-2" href="<?php echo htmlspecialchars(BASE_URL . 'home/index') ?>" style="background-color:#337AB7;" >
+            <div class="mx-5 fw-bold text-white" >Foo Airport </div>    
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,30 +10,34 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <?php if ($isLoggedIn) { ?>
-                    <li class="nav-item">
+                    <!--li class="nav-item">
                         <a class="nav-link active" aria-current="page"
                            href="<?php echo htmlspecialchars(BASE_URL . 'home/index') ?>">
                             Home
                         </a>
-                    </li>
+                    </li-->
                 <?php } ?>
             </ul>
             <ul class="navbar-nav">
                 <?php if ($isLoggedIn) { ?>
                     <li class="nav-item dropdown">
-                        <span class="text-white dropdown-toggle" id="navbarProfileDropdown" role="button"
+                        <span class="dropdown-toggle nav-link" id="navbarProfileDropdown" role="button"
                               data-bs-toggle="dropdown" aria-expanded="false">
                             <?php echo htmlspecialchars($user["name"]) ?>
                         </span>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarProfileDropdown">
                             <li>
                                 <a class="dropdown-item"
+                                       href="">
+                                       <i class="fa-solid fa-user fa-fw"></i> Account
+                                </a>
+                                <a class="dropdown-item"
                                    href="<?php echo htmlspecialchars(BASE_URL . 'auth/logout') ?>">
                                     <i class="fas fa-fw fa-sign-out-alt"></i> Log out
                                 </a>
                             </li>
                         </ul>
-                    </li>
+                    </li-->
                 <?php } else { ?>
                     <li class="nav-item">
                         <a class="nav-link active" href="<?php echo htmlspecialchars(BASE_URL . 'auth/login') ?>">
@@ -41,6 +45,18 @@
                         </a>
                     </li>
                 <?php } ?>
+
+                <!--li class="nav-item">
+                    <a class="nav-link active" href="">
+                        Register
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="">
+                        Login
+                    </a>
+                </li-->
             </ul>
         </div>
     </div>
