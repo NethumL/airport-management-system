fdee55c272c191d9addc083b75be67afb579e834
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-22 16:09:36 +0530

    Implement `/auth/register` route

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -1,9 +1,57 @@
 <?php
+
 require_once __DIR__ . "/../utils.php";
 require_once __DIR__ . "/../models/UserManager.php";
+require_once __DIR__ . "/../models/UnverifiedUserManager.php";
+require_once __DIR__ . "/../EmailHandler.php";
 
 class auth extends Controller
 {
+    public function register()
+    {
+        session_start();
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            if (isset($_SESSION["user"]) && isset($_SESSION["last_activity"])) {
+                redirectRelative("home/index");
+            } else {
+                $data = $this->getViewData();
+                $this->showView("auth/register", $data);
+            }
+        } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $userDetails = filterArrayByKeys($_POST, ["email", "name", "password"]);
+            $undefinedFields = getUnsetKeys($userDetails, ["email", "name", "password"]);
+            if (!empty($undefinedFields)) {
+                create_flash_message("auth/register", "All fields are required.", FLASH_ERROR);
+                redirectRelative("auth/register");
+            }
+
+            if (!filter_var($userDetails["email"], FILTER_VALIDATE_EMAIL)) {
+                create_flash_message("auth/register", "Email is invalid.", FLASH_ERROR);
+                redirectRelative("auth/register");
+            }
+
+            $isRegistered = UserManager::getUserDetails($userDetails["email"]) !== false;
+
+            $isUnverified = UnverifiedUserManager::getUnverifiedUserDetails($userDetails["email"]) !== false;
+
+            if ($isRegistered || $isUnverified) {
+                EmailHandler::sendEmail($userDetails["email"], $userDetails["name"], "Email is already registered", "reregister_email", []);
+                create_flash_message("auth/login", "Check your inbox for a verification email", FLASH_SUCCESS);
+                redirectRelative("auth/login");
+            }
+
+            $result = UnverifiedUserManager::addUnverifiedUser($userDetails["email"], $userDetails["name"], $userDetails["password"]);
+            if ($result) {
+                EmailHandler::sendEmail($userDetails["email"], $userDetails["name"], "Verify email", "email_verification", ['token' => $result]);
+                create_flash_message("auth/login", "Check your inbox for a verification email", FLASH_SUCCESS);
+                redirectRelative("auth/login");
+            } else {
+                create_flash_message("auth/register", "Something went wrong", FLASH_ERROR);
+                redirectRelative("auth/register");
+            }
+        }
+    }
+
     public function login()
     {
         session_start();
