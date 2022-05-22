65a82d44b2b1b0bc4697fdd1c6c979f2d9431e53
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-23 01:03:48 +0530

    Implement `/flight/pay` route

--- app/controllers/flight.php
+++ app/controllers/flight.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . "/../utils.php";
 require_once __DIR__ . "/../models/UserManager.php";
 require_once __DIR__ . "/../models/FlightManager.php";
+require_once __DIR__ . "/../models/BookingManager.php";
+require_once __DIR__ . "/../models/PaymentManager.php";
 
 class flight extends Controller
 {
@@ -18,6 +20,7 @@ class flight extends Controller
         "businessClassPrice",
         "status"
     ];
+    const PAYMENT_FIELDS = ["creditCardNumber", "paidAmount"];
 
     public function index(string $id = "")
     {
@@ -197,11 +200,71 @@ class flight extends Controller
         }
     }
 
+    public function pay(string $id)
+    {
+        session_start();
+        $this->checkAuth("flight/pay", function () {
+            return false;
+        });
+        if ($_SESSION["user"]["userType"] != "CUSTOMER") {
+            redirectRelative("home/index");
+        }
+
+        $booking = BookingManager::getBooking($id);
+        if (!$booking || $booking["email"] != $_SESSION["user"]["email"]) {
+            redirectRelative("home/index");
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $data = $this->getViewData();
+
+            $data["booking"] = $booking;
+
+            $this->showView("flight/pay", $data);
+        } else if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+            $paymentDetails = filterArrayByKeys($_POST, self::PAYMENT_FIELDS);
+
+            $undefinedFields = getUnsetKeys($paymentDetails, self::PAYMENT_FIELDS);
+
+            if (!empty($undefinedFields)) {
+                create_flash_message("flight/pay", "All fields are required.", FLASH_ERROR);
+                redirectRelative("flight/pay/$id");
+            }
+
+            if (!$this->validate_payment_details($paymentDetails)) {
+                create_flash_message("flight/pay", "Fix invalid fields.", FLASH_ERROR);
+                redirectRelative("flight/pay/$id");
+            }
+
+            $result = PaymentManager::addPayment(
+                $paymentDetails["creditCardNumber"],
+                $paymentDetails["paidAmount"],
+                $_SESSION["user"]["email"],
+                $id,
+            );
+            if ($result) {
+                BookingManager::updatePaidStatus($id, true);
+                create_flash_message("home/index", "Payment successful", FLASH_SUCCESS);
+                redirectRelative("home/index");
+            } else {
+                create_flash_message("flight/pay", "Something went wrong.", FLASH_ERROR);
+                redirectRelative("flight/pay/$id");
+            }
+        }
+    }
+
     private function validate_flight_details($details): bool
     {
         if (strlen($details["airline"]) < 1)
             return false;
         if (!ctype_digit($details["begin"]) || !ctype_digit($details["end"]))
+            return false;
+        return true;
+    }
+
+    private function validate_payment_details($details): bool
+    {
+        if (!ctype_digit($details["creditCardNumber"]))
             return false;
         return true;
     }
