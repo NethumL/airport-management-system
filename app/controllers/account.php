eedaef162d2b947e0720b2002276fb3c7cc5cdaf
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-24 18:30:26 +0530

    Implemented ccount/view route

--- app/controllers/account.php
+++ app/controllers/account.php
@@ -3,7 +3,16 @@
 require_once __DIR__ . "/../utils.php";
 require_once __DIR__ . "/../models/UserManager.php";
 
-class account extends Controller
-{
-    
+class account extends Controller {
+
+    public function view() {
+        session_start();
+        $this->checkAuth("account/view", function () {
+            if (!isset($_SESSION["user"])) {
+                redirectRelative("auth/login");
+            }
+            return $this->getViewData();
+        });
+    }
+
 }
