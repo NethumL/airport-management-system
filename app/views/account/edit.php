854248f25464021b45c8ab9e384aa65864c391a7
Sathira443 <liyanapathiranasathira@gmail.com>
2022-05-27 22:41:14 +0530

    Save and Edit Buttons Renamed

--- app/views/account/edit.php
+++ app/views/account/edit.php
@@ -107,7 +107,7 @@
                         <div class="row">
                             <div class="col-md-9 col-sm-9"></div>
                             <div class="col-md-3 col-sm-3">
-                                <button type="submit" class="btn btn-primary fw-bold">Send</button>
+                                <button type="submit" class="btn btn-primary fw-bold">Save</button>
                             </div>
 
                         </div>
