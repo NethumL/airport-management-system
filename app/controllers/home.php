e5a5d4fdc2cefc442d99276792f5defddbb77f59
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-28 23:59:06 +0530

    Fix implementation of `/home/index` route

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . "/../utils.php";
 require_once __DIR__ . "/../models/BookingManager.php";
 require_once __DIR__ . "/../models/FlightManager.php";
+require_once __DIR__ . "/../models/SeatManager.php";
 
 class home extends Controller
 {
@@ -10,29 +11,37 @@ class home extends Controller
     {
         session_start();
 
-        $data = [];
-
         if (isset($_SESSION['user'])) {
-            $this->checkAuth("home/index", function() {
-                return false;
+            $this->checkAuth("home/index", function () {
+                $data = $this->getViewData();
+                $user = $_SESSION["user"];
+
+                if ($user["userType"] === "CUSTOMER") {
+                    $result = BookingManager::getBookingBy($user["email"]);
+                    $bookings = [];
+                    foreach ($result as $booking) {
+                        $flight = FlightManager::getFlight($booking["flightNumber"]);
+                        $seats = [];
+                        foreach ($booking['seats'] as $seatId) {
+                            $seats[] = SeatManager::getSeat($seatId);
+                        }
+                        $bookings[] = [...$booking, "flight" => $flight, "seats" => $seats];
+                    }
+                    $data["bookings"] = $bookings;
+                }
+                return $data;
             });
-
-            $data = $this->getViewData();
-            $user = $_SESSION["user"];
-
-            if ($user["userType"] === "CUSTOMER") {
-                $data["bookings"] = BookingManager::getBookingBy($user["email"]);
-            } else if (in_array($_SESSION["user"]["userType"], ["EMPLOYEE", "MANAGER"], true)) {
-                $startDate = date("Y-m-d");
-
-                $date = date_create($startDate);
-                date_add($date,date_interval_create_from_date_string("3 days"));
-                $endDate =  date_format($date,"Y-m-d");
-
-                $data["flights"] = FlightManager::getFlightsBy("", "", $startDate, $endDate);
-            }
-
         }
+
+        $data = $this->getViewData();
+
+        $startDate = date("Y-m-d");
+
+        $date = date_create($startDate);
+        date_add($date, date_interval_create_from_date_string("3 days"));
+        $endDate = date_format($date, "Y-m-d");
+
+        $data["flights"] = FlightManager::getFlightsBy("", "", $startDate, $endDate);
 
         $this->showView("home/index", $data);
     }
