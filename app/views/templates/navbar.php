6587f210e3de1aafcea2c00a9cfe1af8ad89be7a
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-28 18:57:58 +0530

    Add navigation links to navbar

--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -14,13 +14,38 @@ if (empty($user)) {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <?php if ($isLoggedIn) { ?>
-                    <!--li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                           href="<?php echo htmlspecialchars(BASE_URL . 'home/index') ?>">
-                            Home
+                <?php if (isset($user) && $user["userType"] == "MANAGER") { ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarAdminDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Admin
                         </a>
-                    </li-->
+                        <ul class="dropdown-menu" aria-labelledby="navbarAdminDropdown">
+                            <li><a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'admin/new') ?>">Add new</a></li>
+                            <li><a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'admin/edit') ?>">Edit</a></li>
+                        </ul>
+                    </li>
+                <?php } ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarFlightDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Flights
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarFlightDropdown">
+                        <?php if (isset($user) && in_array($user["userType"], ["MANAGER", "EMPLOYEE"])) { ?>
+                            <li><a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'flight/new') ?>">Add new</a></li>
+                        <?php } ?>
+                        <li><a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'flight/index') ?>">View all</a></li>
+                    </ul>
+                </li>
+                <?php if (isset($user) && in_array($user["userType"], ["MANAGER", "EMPLOYEE"])) { ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarAirportDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Airports
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarAirportDropdown">
+                            <li><a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'airport/new') ?>">Add new</a></li>
+                            <li><a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'airport/edit') ?>">Edit</a></li>
+                        </ul>
+                    </li>
                 <?php } ?>
             </ul>
             <ul class="navbar-nav">
@@ -31,7 +56,7 @@ if (empty($user)) {
                         </span>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarProfileDropdown">
                             <li>
-                                <a class="dropdown-item" href="">
+                                <a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'account/view') ?>">
                                     <i class="fa-solid fa-user fa-fw"></i> Account
                                 </a>
                                 <a class="dropdown-item" href="<?php echo htmlspecialchars(BASE_URL . 'auth/logout') ?>">
