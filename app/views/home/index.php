bd457c31975a8683a860c85c8b4d60d8e8c7b3e9
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-28 19:19:05 +0530

    Fix styling in `/home/index` view

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -15,33 +15,36 @@ showNavbar($data);
 ?>
 
 <main style="margin-top: 58px">
-<div style="margin-top: 150px;">   
-        <div class="m-5 fw-bold text-white fs-1 text-center" >
-            <span class= "p-5" style="background-color:#337AB7;">Foo Airport</span>
-        </div>
-    </div>
-    <div class=" pt-5"></div>
-
-    <div class="card col-sm-4 mx-auto ">
-        <div class="card-header text-white" style="background-color:#337AB7;">
+    <h1 class="text-center" style="margin-top: 80px;"><?php echo htmlspecialchars($_ENV["AIRPORT_NAME"]) ?></h1>
+    <div class="card col-sm-4 mx-auto mt-5">
+        <div class="card-header text-white bg-primary">
           Quick Links
         </div>
-        <div class="card-body mb-2">
-                <button type="button" class="col-lg-2 btn text-white ms-5 mt-1 col-md-8 col-sm-8" style="background-color:#5AC0DE;">Search</button>
-                <button type="button" class="col-lg-4 btn text-white ms-4 me-4 mt-1 col-md-10 col-sm-10" style="background-color:#5AC0DE;">Register new account</button>
-                <button type="button" class="col-lg-3 btn text-white mt-1 col-md-12 col-sm-12" style="background-color:#5AC0DE;">Edit schedules</button>
+        <div class="card-body mb-2 d-flex justify-content-evenly">
+            <a type="button" class="btn btn-info text-white" href="<?php echo htmlspecialchars(BASE_URL . 'flight/index') ?>">
+                View all flights
+            </a>
+            <?php if (!isset($data["user"])) { ?>
+                <a type="button" class="btn btn-info text-white" href="<?php echo htmlspecialchars(BASE_URL . 'auth/register') ?>">
+                    Register new account
+                </a>
+            <?php } ?>
         </div>
     </div>
 
-    <div class=" pt-3"></div>
-
     <div class="card col-sm-5 mx-auto mt-5">
-        <div class="card-header text-white " style="background-color:#337AB7;">
-          New flights
-          <button type="button" class="col-md-3 col-sm-6 btn text-white ms-5 me-4 float-end" style="background-color:#5AC0DE;">View all</button>
+        <div class="card-header text-white bg-primary">
+          <?php if (isset($data["user"]) && in_array($data["user"]["userType"], ["MANAGER", "EMPLOYEE"], true)) { ?>
+              New flights
+              <a class="btn btn-info text-white float-end" href="<?php echo htmlspecialchars(BASE_URL . 'flight/index') ?>">
+                  View all
+              </a>
+          <?php } else { ?>
+              Booked flights
+          <?php }  ?>
         </div>
-        <div class="card-body mb-2">
-            <div class="table-responsive mx-5">
+        <div class="card-body">
+            <div class="table-responsive">
                 <table class="table w-auto">
                     <thead>
                       <tr>
