ce0ee95d916800e55b58a68c13eb5ce5ce022331
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-29 02:16:59 +0530

    Bind data to `/flight/book` view

--- app/views/flight/book.php
+++ app/views/flight/book.php
@@ -7,14 +7,9 @@
     <title>Book</title>
     <?php include __DIR__ . "/../templates/head.php" ?>
     <style>
-        .login-form form {
-            background: white;
-        }
-
         .text {
             text-align: right;
         }
-
 
         .btn {
             width: 100%;
@@ -35,116 +30,132 @@
     ?>
 
     <main style="margin-top: 150px">
-        <div class="login-form d-flex justify-content-center" style="margin-top: 63px;">
-
-
+        <div class="d-flex justify-content-center" style="margin-top: 63px;">
             <div class="card col-md-5 col-sm-7 mx-auto ">
                 <div class="card-header text-white bg-primary">
                     Book
                 </div>
                 <div class="card-body border-primary">
-                    <form action="pay" method="post">
-                         <!-- Airline --> 
-                         <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text fw-bold ">
-                                <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label">Airline:</label>
-                            </div>
-
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label">Srilankan Airlines</label>
-                            </div>
-                           
-                        </div>
-                        <!-- From -->
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text fw-bold ">
-                                <label for="from" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">From:</label>
-                            </div>
-
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="from" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Colombo</label>
-                            </div>
+                    <!-- Airline -->
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 rounded text fw-bold ">
+                            <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label">Airline:</label>
                         </div>
 
-                        <!-- To -->
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text fw-bold ">
-                                <label for="to" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">To:</label>
-                            </div>
-
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="to" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Jaffna</label>
-                            </div>
-                        
+                        <div class="col-md-6 col-sm-6 rounded">
+                            <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label">
+                                <?php echo htmlspecialchars($data["flight"]["airline"]) ?>
+                            </label>
                         </div>
 
-                        <!-- Departure -->
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text fw-bold ">
-                                <label for="departure" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Departure:</label>
-                            </div>
-
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="departure" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">24/05/2022 5:00</label>
-                            </div>
+                    </div>
+                    <!-- From -->
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 rounded text fw-bold ">
+                            <label for="from" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">From:</label>
                         </div>
 
-                        <!-- Arrival-->
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text fw-bold ">
-                                <label for="arrival" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Arrival:</label>
-                            </div>
-
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="arrival" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">25/05/2022 5:00</label>
-                            </div>
+                        <div class="col-md-6 col-sm-6 rounded">
+                            <label for="from" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">
+                                <?php echo htmlspecialchars($data["flight"]["beginName"]) ?>
+                            </label>
                         </div>
-                        <!-- Select Seats -->
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text fw-bold ">
-                                <label for="selectSeats" class="col-md-8  col-sm-8 me-1 rounded-2 col-form-label">Select Seats</label>
-                            </div>
+                    </div>
 
-                            <div class="col-md-7 col-sm-7 rounded mt-2">
-                             
-                                <fieldset >
-                                    <p class="fw-bold">Economy</p>
-                                    <?php
-                                    for ($x = 1; $x <= 30; $x++) {
-                                    ?>
-                                    <div class="form-check form-check-inline" style="width:10px; margin-right:30px">
-                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox" value="option">
-                                        <label class="form-check-label" for="inlineCheckbox"><?=$x;?></label>
-                                    </div>
-                                    <?php 
-                                    }
-                                    ?>
-                                </fieldset>
-                               
-                                <fieldset>
-                                    <p class="fw-bold mt-2">Business</p>
-                                    <?php
-                                    for ($x = 31; $x <= 60; $x++) {
-                                    ?>
-                                    <div class="form-check form-check-inline" style="width:10px; margin-right:30px">
-                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox" value="option">
-                                        <label class="form-check-label" for="inlineCheckbox"><?=$x;?></label>
-                                    </div>
-                                    <?php 
-                                    }
-                                    ?>
-                                </fieldset>
-
-                            </div>
-                           
-                            
+                    <!-- To -->
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 rounded text fw-bold ">
+                            <label for="to" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">To:</label>
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-7 col-sm-7"></div>
+                        <div class="col-md-6 col-sm-6 rounded">
+                            <label for="to" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">
+                                <?php echo htmlspecialchars($data["flight"]["endName"]) ?>
+                            </label>
+                        </div>
+                    </div>
 
+                    <!-- Departure -->
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 rounded text fw-bold ">
+                            <label for="departure" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Departure:</label>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 rounded">
+                            <label for="departure" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">
+                                <?php echo htmlspecialchars($data["flight"]["departureDateTime"]) ?>
+                            </label>
+                        </div>
+                    </div>
+
+                    <!-- Arrival-->
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 rounded text fw-bold ">
+                            <label for="arrival" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Arrival:</label>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 rounded">
+                            <label for="arrival" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">
+                                <?php echo htmlspecialchars($data["flight"]["arrivalDateTime"]) ?>
+                            </label>
+                        </div>
+                    </div>
+
+                    <!-- Select Seats -->
+                    <form action="<?php echo htmlspecialchars(BASE_URL . 'flight/book/' . $data['flight']['id']) ?>"
+                          method="post" class="row">
+                        <div class="col-md-4 col-sm-4 rounded text fw-bold ">
+                            <label for="selectSeats" class="col-md-8  col-sm-8 me-1 rounded-2 col-form-label">Select Seats</label>
+                        </div>
+
+                        <div class="col-md-7 col-sm-7 rounded mt-2">
+                            <fieldset>
+                                <p class="fw-bold">Economy</p>
+                                <?php foreach ($data["seats"] as $seat) { ?>
+                                    <?php if ($seat["class"] == "ECONOMY") { ?>
+                                        <div class="form-check form-check-inline" style="width:10px; margin-right:30px">
+                                            <input class="form-check-input" type="checkbox"
+                                                   id="seat-<?php echo htmlspecialchars($seat['id']) ?>"
+                                                   name="seat-<?php echo htmlspecialchars($seat['id']) ?>"
+                                                   value="seat-<?php echo htmlspecialchars($seat['id']) ?>"
+                                                <?php if ($seat['isBooked']) { ?>
+                                                    disabled
+                                                <?php } ?>
+                                            >
+                                            <label class="form-check-label"
+                                                   for="seat-<?php echo htmlspecialchars($seat['id']) ?>">
+                                                <?php echo htmlspecialchars($seat["xPosition"] . $seat["yPosition"]) ?>
+                                            </label>
+                                        </div>
+                                    <?php } ?>
+                                <?php } ?>
+                            </fieldset>
+
+                            <fieldset>
+                                <p class="fw-bold mt-2">Business</p>
+                                <?php foreach ($data["seats"] as $seat) { ?>
+                                    <?php if ($seat["class"] == "BUSINESS") { ?>
+                                        <div class="form-check form-check-inline" style="width:10px; margin-right:30px">
+                                            <input class="form-check-input" type="checkbox"
+                                                   id="seat-<?php echo htmlspecialchars($seat['id']) ?>"
+                                                   name="seat-<?php echo htmlspecialchars($seat['id']) ?>"
+                                                   value="seat-<?php echo htmlspecialchars($seat['id']) ?>"
+                                                <?php if ($seat['isBooked']) { ?>
+                                                    disabled
+                                                <?php } ?>>
+                                            <label class="form-check-label"
+                                                   for="inlineCheckbox">
+                                                <?php echo htmlspecialchars($seat["xPosition"] . $seat["yPosition"]) ?>
+                                            </label>
+                                        </div>
+                                    <?php } ?>
+                                <?php } ?>
+                            </fieldset>
+                        </div>
+
+                        <div class="row mt-3">
                             <!-- book button -->
-                            <div class="col-md-4 col-sm-4">
+                            <div class="col-md-4 offset-md-7 col-sm-4 offset-sm-7">
                                 <button type="submit" class="btn btn-success fw-bold">Book</button>
                             </div>
                         </div>
