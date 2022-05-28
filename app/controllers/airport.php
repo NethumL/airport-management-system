108e346acd5fc92380b766535338a67847c1107d
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-28 17:33:05 +0530

    Implement `airport` controller

---
+++ app/controllers/airport.php
@@ -0,0 +1,139 @@
+<?php
+
+require_once __DIR__ . "/../utils.php";
+require_once __DIR__ . "/../models/AirportManager.php";
+
+class airport extends Controller
+{
+    public function new()
+    {
+        session_start();
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $this->checkAuth("airport/new", function () {
+                if (!in_array($_SESSION["user"]["userType"], ["MANAGER", "EMPLOYEE"], true)) {
+                    redirectRelative("home/index");
+                }
+                return $this->getViewData();
+            });
+        } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->checkAuth("airport/new", function () {
+                if (!in_array($_SESSION["user"]["userType"], ["MANAGER", "EMPLOYEE"], true)) {
+                    redirectRelative("home/index");
+                }
+            });
+
+            $name = $_POST["name"];
+
+            if (empty($name)) {
+                create_flash_message("airport/new", "Name is required.", FLASH_ERROR);
+                redirectRelative("airport/new");
+            }
+
+            if (strlen($name) < 1) {
+                create_flash_message("airport/new", "Name is invalid.", FLASH_ERROR);
+                redirectRelative("airport/new");
+            }
+
+            $result = AirportManager::registerAirport($name);
+
+            if ($result)
+                create_flash_message("airport/new", "Airport added successfully.", FLASH_SUCCESS);
+            else
+                create_flash_message("airport/new", "Something went wrong", FLASH_ERROR);
+
+            redirectRelative("airport/new");
+        }
+    }
+
+    public function edit(string $id = "")
+    {
+        session_start();
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $this->checkAuth("airport/edit", function () {
+                return false;
+            });
+
+            if (!in_array($_SESSION["user"]["userType"], ["MANAGER", "EMPLOYEE"], true)) {
+                http_response_code(403);
+                die();
+            }
+
+            $data = $this->getViewData();
+
+            if (empty($id)) {
+                $data["airports"] = AirportManager::getAllAirports();
+            } else {
+                $airport = AirportManager::getAirport($id);
+
+                if ($airport)
+                    $data["airport"] = $airport;
+                else {
+                    http_response_code(404);
+                    die();
+                }
+            }
+
+            $this->showView("airport/edit", $data);
+        } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->checkAuth("airport/edit", function () {
+                if (!in_array($_SESSION["user"]["userType"], ["MANAGER", "EMPLOYEE"], true)) {
+                    http_response_code(403);
+                    die;
+                }
+            });
+
+            if (empty($id)) {
+                http_response_code(400);
+                die();
+            }
+
+            $result = AirportManager::getAirport($id);
+            if (!$result) {
+                http_response_code(404);
+                die();
+            }
+
+            $name = $_POST["name"];
+
+            if (empty($name) || strlen($name) < 1) {
+                http_response_code(400);
+                die();
+            }
+
+            $result = AirportManager::updateAirport($id, $name);
+
+            if ($result) {
+                $response = AirportManager::getAirport($id);
+                echo json_encode($response);
+            } else {
+                http_response_code(500);
+                die();
+            }
+        } else if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+            $this->checkAuth("airport/edit", function () {
+                if (!in_array($_SESSION["user"]["userType"], ["MANAGER", "EMPLOYEE"], true)) {
+                    http_response_code(403);
+                    die();
+                }
+                return $this->getViewData();
+            });
+
+            if (empty($id)) {
+                http_response_code(400);
+                die();
+            }
+
+            $result = AirportManager::getAirport($id);
+            if (!$result) {
+                http_response_code(404);
+                die();
+            }
+
+            $result = AirportManager::removeAirport($id);
+            if (!$result) {
+                http_response_code(400);
+                die();
+            }
+        }
+    }
+}
