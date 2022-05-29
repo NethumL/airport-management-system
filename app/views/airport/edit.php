44114ecbb2fbb677037e6e3c94b3d4b3cec65703
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-29 20:21:46 +0530

    Fix and bind data to `/airport/edit` view

--- app/views/airport/edit.php
+++ app/views/airport/edit.php
@@ -15,7 +15,12 @@ showNavbar($data);
 ?>
 
 <main style="margin-top: 58px; margin-left: 10%">
-<div class="row justify-content-center ms-5"> 
+    <div style="margin-top: 70px">
+        <?php display_flash_message("flight/index"); ?>
+    </div>
+    <div id="alert-container">
+    </div>
+<div class="row justify-content-center ms-5">
     <div style="margin-top: 63px;">   
         <div class="px-5 py-4 fw-bold h5 w-25" >
             <span class= "mx-5  py-2 px-3 rounded-3">Filter</span>
@@ -39,7 +44,7 @@ showNavbar($data);
     </div>
     
     <div class="table-responsive">
-    <table class="table w-auto " style="margin-left:35%">
+    <table class="table w-auto" id="airport-table" style="margin-left:35%">
         <thead>
           <tr>
             <th class="pe-5" scope="col">Airport Name<i class="ms-1 fa-solid fa-sort"></i></th>
@@ -47,61 +52,39 @@ showNavbar($data);
           </tr>
         </thead>
         <tbody>
-          <tr > 
-            <td>Colombo Airport</td>
+        <?php if (isset($data["airport"])) { ?>
+          <tr data-id="<?php echo htmlspecialchars($data['airport']['id']) ?>">
+            <td><?php echo htmlspecialchars($data["airport"]["name"]) ?></td>
             <td><button type="button" class="btn"><i class="fa-solid fa-pen-to-square fa-1x"></i></i></button></td>
           </tr>
-          <tr > 
-            <td>Colombo Airport</td>
-            <td><button type="button" class="btn"><i class="fa-solid fa-pen-to-square fa-1x"></i></i></button></td>
-          </tr>
-          <tr > 
-            <td>Colombo Airport.</td>
-            <td><button type="button" class="btn"><i class="fa-solid fa-pen-to-square fa-1x"></i></i></button></td>
-          </tr>
-          <tr > 
-            <td>Colombo Airport</td>
-            <td><button type="button" class="btn"><i class="fa-solid fa-pen-to-square fa-1x"></i></i></button></td>
-          </tr>
-          <tr > 
-            <td>Colombo Airport</td>
-            <td><button type="button" class="btn"><i class="fa-solid fa-pen-to-square fa-1x"></i></i></button></td>
-          </tr>
-          <tr > 
-            <td>Colombo Airport</td>
-            <td><button type="button" class="btn"><i class="fa-solid fa-pen-to-square fa-1x"></i></i></button></td>
-          </tr>
+        <?php } ?>
         </tbody>
       </table>
     </div>
-    <div class="ms-5">
-        <span class="ms-5"><a class="mx-5 fw-bold btn btn-success" href="new" role="button">Add New Airport</a><span>
-    </div>
-    
-    <div class= "m-5">
-        <form method="POST">  
+
+    <div class="m-5">
+        <form method="POST" id="update-form">
             <div class="row col-md-10 col-sm-10">
               <div class="row col-md-4  col-sm-5 form-group m-3 mx-auto">
                   <label for="name" class="col-md-4  col-sm-5 ms-5 me-1 rounded-2 col-form-label">Name</label>
                   <div class="col-md-6 col-sm-6 rounded">
-                      <input type="text" class="form-control" id="name">
+                      <input type="text" class="form-control" id="name" name="name">
                   </div>
               </div>    
-              <div class="row col-md-8  col-sm-5 form-group mx-auto">
-                  <div class="col-md-10 ">
-                    <button type="button" class="btn btn-success mx-3 mt-2 float-end">Update</button>
-                    <button type="button" class="btn btn-warning mx-3 mt-2 float-end">Clear</button>
-                    <button type="button" class="btn btn-danger mx-3 mt-2 float-end">Delete</button>
+              <div class="row form-group mx-auto mt-3">
+                  <div class="col-md-10">
+                    <button id="update-button" type="submit" class="btn btn-success mx-3 mt-2 float-end">Update</button>
+                    <button id="clear-button" type="reset" class="btn btn-warning mx-3 mt-2 float-end">Clear</button>
+                    <button id="delete-button" type="button" class="btn btn-danger mx-3 mt-2 float-end">Delete</button>
                 </div>
               </div>
             </div>
-              
         </form>
    </div>
 </div>
 
- 
 </main>
+
 </body>
 
 </html>
