2d57e73d8d759afe116b0720797cab302d497a7e
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-29 17:01:20 +0530

    Fix and bind data to `/flight/pay` view

--- app/views/flight/pay.php
+++ app/views/flight/pay.php
@@ -7,10 +7,6 @@
     <title>Payment</title>
     <?php include __DIR__ . "/../templates/head.php" ?>
     <style>
-        .login-form form {
-            background: white;
-        }
-
         .text {
             text-align: right;
         }
@@ -34,9 +30,7 @@
     ?>
 
     <main style="margin-top: 150px">
-        <div class="login-form d-flex justify-content-center" style="margin-top: 63px;">
-
-
+        <div class="d-flex justify-content-center" style="margin-top: 63px;">
             <div class="card col-md-5 col-sm-7 col-xs-9 mx-auto ">
                 <div class="card-header text-white bg-primary">
                     Pay
@@ -48,16 +42,17 @@
                         <label for="bookingDetails" class="col-md-8  col-sm-5 rounded-2 col-form-label"><h6>Booking Details</h6></label>
                     </div>
                      
-                    <form action="home/index" method="post">
-
+                    <form action="<?php echo htmlspecialchars(BASE_URL . 'flight/pay/' . $data['booking']['id']) ?>" method="post">
                         <!-- email -->
                         <div class="row">
                             <div class="col-md-4 col-sm-4 rounded text">
                                 <label for="email" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Email</label>
                             </div>
 
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="email" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Laksi@gmail.com</label>
+                            <div class="col-md-8 col-sm-8 rounded">
+                                <label for="email" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">
+                                    <?php echo htmlspecialchars($data["user"]["email"]) ?>
+                                </label>
                             </div>
                            
                         </div>
@@ -65,11 +60,13 @@
                         <!-- Name -->
                         <div class="row">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="name" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Name</label>
+                                <label for="name" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">Name</label>
                             </div>
 
                             <div class="col-md-6 col-sm-6 rounded">
-                                <label for="name" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Laksika T</label>
+                                <label for="name" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">
+                                    <?php echo htmlspecialchars($data["user"]["name"]) ?>
+                                </label>
                             </div>
                             
                         </div>
@@ -77,24 +74,13 @@
                          <!-- Airline --> 
                         <div class="row">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label fw-bold ">Airline:</label>
+                                <label for="airline" class="col-md-8 col-sm-5 rounded-2 col-form-label fw-bold">Airline</label>
                             </div>
 
                             <div class="col-md-6 col-sm-6 rounded">
-                                <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label">Srilankan Airlines</label>
-                            </div>
-                           
-                        </div>
-
-
-                        <!-- Class-->
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="class" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Class:</label>
-                            </div>
-
-                            <div class="col-md-6 col-sm-6 rounded">
-                                <label for="class" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">Business</label>
+                                <label for="airline" class="col-md-8  col-sm-5 rounded-2 col-form-label">
+                                    <?php echo htmlspecialchars($data["flight"]["airline"]) ?>
+                                </label>
                             </div>
                            
                         </div>
@@ -102,11 +88,18 @@
                         <!-- Seat -->
                         <div class="row">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="seatNumbers" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Seat Numbers</label>
+                                <label for="seatNumbers" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">Seat Numbers</label>
                             </div>
 
                             <div class="col-md-6 col-sm-6 rounded">
-                                <label for="seatNumbers" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">31,32</label>
+                                <label for="seatNumbers" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label">
+                                    <?php
+                                    $seatNames = array_map(function ($seat) {
+                                        return $seat['xPosition'] . $seat['yPosition'];
+                                    }, $data["seats"]);
+                                    echo htmlspecialchars(join(", ", $seatNames));
+                                    ?>
+                                </label>
                             </div>
                           
                         </div>
@@ -114,22 +107,24 @@
                         <!-- Payment Amount -->
                         <div class="row mb-3">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="payment" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Payment Amount</label>
+                                <label for="payment" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">Payment Amount</label>
                             </div>
 
                             <div class="col-md-6 col-sm-6 rounded">
-                                <label for="payment" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label">6000</label>
+                                <label for="payment" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label">
+                                    Rs. <?php echo htmlspecialchars($data["amount"]) ?>
+                                </label>
                             </div>
                         </div>
 
                         <div class="col-md-4 col-sm-4 rounded ms-5">
-                            <label for="email" class="col-md-8  col-sm-5 rounded-2 col-form-label"><h6>Card Details</h6></label>
+                            <label for="email" class="col-md-8 col-sm-5 rounded-2 col-form-label"><h6>Card Details</h6></label>
                         </div>
 
                          <!-- Card holder name-->
                          <div class="row mb-5">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="cardName" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Card Holder Name</label>
+                                <label for="cardName" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">Card Holder Name</label>
                             </div>
 
                             <div class="col-md-7 col-sm-7 rounded">
@@ -140,19 +135,20 @@
                          <!-- Card number-->
                          <div class="row mb-5">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="cardNumber" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Card Number</label>
+                                <label for="creditCardNumber" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">
+                                    Card Number
+                                </label>
                             </div>
 
                             <div class="col-md-7 col-sm-7 rounded">
-                                <input type="text" class="form-control" name="cardNumber" id="cardNumber" required="required">
+                                <input type="text" class="form-control" name="creditCardNumber" id="creditCardNumber" required="required">
                             </div>
-                           
                         </div>
 
                         <!-- Expiry Date-->
                         <div class="row mb-5">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="expiryDate" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">Expiry Date</label>
+                                <label for="expiryDate" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">Expiry Date</label>
                             </div>
 
                             <div class="col-md-7 col-sm-7 rounded">
@@ -161,29 +157,28 @@
                            
                         </div>
 
-                        <!-- CNN-->
+                        <!-- CVV -->
                         <div class="row mb-5">
                             <div class="col-md-4 col-sm-4 rounded text">
-                                <label for="cnn" class="col-md-8  col-sm-5 me-1 rounded-2 col-form-label fw-bold ">CNN</label>
+                                <label for="cvv" class="col-md-8 col-sm-5 me-1 rounded-2 col-form-label fw-bold">CVV</label>
                             </div>
 
                             <div class="col-md-7 col-sm-7 rounded">
-                                <input type="text" class="form-control" name="cnn" id="cnn" required="required">
+                                <input type="text" class="form-control" name="cvv" id="cvv" required="required">
                             </div>
-                           
                         </div>
-
-
-
 
                         <div class="row">
                             <div class="col-md-4 col-sm-4">
-                               <a class="btn btn-warning" href="home/index" role="button">Pay Later</a>
+                                <a class="btn btn-warning"
+                                   href="<?php echo htmlspecialchars(BASE_URL . 'home/index') ?>" role="button">
+                                    Pay Later
+                                </a>
                             </div>
                             <div class="col-md-4 col-sm-4">
                                 
                             </div>
-                            <!-- regitser button -->
+                            <!-- pay button -->
                             <div class="col-md-4 col-sm-4">
                                 <button type="submit" class="btn btn-success fw-bold">Pay Now</button>
                             </div>
